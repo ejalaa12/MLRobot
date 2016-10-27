88d7d59294a6e4939803677a9070762d50e38e60
El Jawad Alaa <ejalaa12@gmail.com>
2016-10-27 18:14:19 +0200

    added dt as parameter, and class is now abstract

--- models/model.py
+++ models/model.py
@@ -1,21 +1,25 @@
+import abc
 from numpy import array
 
 
 class SimulationModel(object):
     """Abstract class for models for simulation"""
 
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=0, y=0, dt=0.1):
         super(SimulationModel, self).__init__()
         self.x = 0
         self.y = 0
         self.X = array([self.x, self.y])
+        self.dt = dt
 
+    @abc.abstractmethod
     def draw(self):
         " Function to draw the model at its current position"
-        pass
 
-    def simulate(self, u):
-        pass
+    @abc.abstractmethod
+    def simulate(self, u, env_conditions):
+        "Simulates the model with a command u for dt"
 
+    @abc.abstractmethod
     def fdot(self, u):
-        pass
+        "How to compute the evolution function of the model"
